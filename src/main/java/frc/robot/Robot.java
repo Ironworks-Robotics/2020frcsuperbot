ef42374d8bc76bd7a3d689545382cc7d3b9bae35
Jason Yuan <jason.yuan869@gmail.com>
2020-03-05 14:45:30 -0500

    new branch from master, removed colorsensors

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,17 +1,13 @@
 package frc.robot;
 
-import com.revrobotics.ColorSensorV3;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-import com.revrobotics.ColorMatchResult;
 import com.revrobotics.CANSparkMax;
-import com.revrobotics.ColorMatch;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.networktables.*;
 import edu.wpi.first.wpilibj.*;
@@ -132,25 +128,10 @@ public class Robot extends TimedRobot {
 
   /** IR Sensor (intake) */
   DigitalInput intakeIR = new DigitalInput(2);
-
-  /** Color Sensor */
-  private final I2C.Port i2cPort = I2C.Port.kOnboard;
-  private final ColorSensorV3 m_colorSensor = new ColorSensorV3(i2cPort);
-  private final ColorMatch m_colorMatcher = new ColorMatch();
-
-  private final Color kBlueTarget = ColorMatch.makeColor(0.143, 0.427, 0.429);
-  private final Color kGreenTarget = ColorMatch.makeColor(0.197, 0.561, 0.240);
-  private final Color kRedTarget = ColorMatch.makeColor(0.441, 0.432, 0.174);
-  private final Color kYellowTarget = ColorMatch.makeColor(0.361, 0.524, 0.113);
-
   /* *****************ROBOT INIT***************** */
   @Override
   public void robotInit() {
     gyroBoy.calibrate();
-    m_colorMatcher.addColorMatch(kBlueTarget);
-    m_colorMatcher.addColorMatch(kGreenTarget);
-    m_colorMatcher.addColorMatch(kRedTarget);
-    m_colorMatcher.addColorMatch(kYellowTarget);
 
     lights.set(lightsOn);
 
@@ -256,23 +237,6 @@ public class Robot extends TimedRobot {
     if (angle < 0) {
       int angleNum = (int) angle % -360;
       angle += 360 * (angleNum + 1);
-    }
-
-    /** Colour sensor */
-    Color detectedColor = m_colorSensor.getColor();
-    String colorString;
-    ColorMatchResult match = m_colorMatcher.matchClosestColor(detectedColor);
-
-    if (match.color == kBlueTarget) {
-      colorString = "Blue";
-    } else if (match.color == kRedTarget) {
-      colorString = "Red";
-    } else if (match.color == kGreenTarget) {
-      colorString = "Green";
-    } else if (match.color == kYellowTarget) {
-      colorString = "Yellow";
-    } else {
-      colorString = "Unknown";
     }
 
     /******************************
@@ -413,12 +377,6 @@ public class Robot extends TimedRobot {
     SmartDashboard.putNumber("Angle: ", angle);
     SmartDashboard.putNumber("Rate: ", rate);
     SmartDashboard.putBoolean("gyro Connected: ", gyroConnected);
-
-    SmartDashboard.putNumber("Red: ", detectedColor.red);
-    SmartDashboard.putNumber("Green: ", detectedColor.green);
-    SmartDashboard.putNumber("Blue: ", detectedColor.blue);
-    SmartDashboard.putNumber("Confidence: ", match.confidence);
-    SmartDashboard.putString("Detected Color: ", colorString);
 
     SmartDashboard.putBoolean("Safety: ", safety);
     SmartDashboard.putBoolean("Reverse: ", reverseControls);
