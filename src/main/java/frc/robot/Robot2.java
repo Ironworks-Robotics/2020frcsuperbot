dde1021a288ff3c237a40ac6e9b53eddf80201ed
Jason Yuan <jason.yuan869@gmail.com>
2020-03-10 00:49:21 -0400

    added auto

--- src/main/java/frc/robot/Robot2.java
+++ src/main/java/frc/robot/Robot2.java
@@ -1,13 +1,10 @@
 package frc.robot;
 
-import com.ctre.phoenix.motorcontrol.ControlMode;
-
 import edu.wpi.cscore.UsbCamera;
 import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.PowerDistributionPanel;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.XboxController;
@@ -17,13 +14,10 @@ import frc.robot.subsystems.Motors;
 import frc.robot.subsystems.Vision;
 
 public class Robot2 extends TimedRobot {
-    /* PDP */
-    PowerDistributionPanel pdp;
-    double[] current;
-
     /* CONTROLLERS */
     XboxController _gamepadDrive; // Xbox controller to drive, elevator
     Joystick _gamepadShoot; // PS4 controller for turret, intake
+    boolean manualOverride;
 
     /* AUTO */
     Timer timer;
@@ -32,6 +26,7 @@ public class Robot2 extends TimedRobot {
     ADXRS450_Gyro gyroBoy = new ADXRS450_Gyro();
     double angle, rate;
     boolean gyroConnected;
+    int angleNum;
 
     /* DRIVE */
     double forward, turn;
@@ -51,9 +46,6 @@ public class Robot2 extends TimedRobot {
         
         camera = CameraServer.getInstance().startAutomaticCapture(0);
 
-        pdp = new PowerDistributionPanel();
-        current = new double[16];
-
         Vision.init();
         Motors.init();
         Drivetrain.init(false, true);
@@ -62,9 +54,18 @@ public class Robot2 extends TimedRobot {
 
     @Override
     public void robotPeriodic(){
-        for (int i = 0; i < current.length; i++) {
-            current[i] = pdp.getCurrent(i);
-        }
+        angle = gyroBoy.getAngle();
+        rate = gyroBoy.getRate();
+        gyroConnected = gyroBoy.isConnected();
+        if (angle > 360) {
+            angleNum = (int) angle % 360;
+            angle -= 360 * angleNum;
+          }
+      
+          if (angle < 0) {
+            angleNum = (int) angle % -360;
+            angle += 360 * (angleNum + 1);
+          }
 
         Vision.periodic();
         Dashboard.periodic();
@@ -78,15 +79,40 @@ public class Robot2 extends TimedRobot {
 
     @Override
     public void autonomousPeriodic(){
-        if (timer.get() < 5) {
+        if (timer.get() <= 5) {
             Drivetrain.drivePeriodic(0.5, 0);
         } else {
             Drivetrain.drivePeriodic(0, 0);
         }
 
         if (timer.get() > 5) {
-            
+            if (Motors.aimTurret()){
+                Motors.loadTurret(true); // begin loading turret
+            } else if (timer.get() > 10){
+                Motors.loadTurret(false); // stop all motors 
+                Motors.flyOff();
+            }
         }
+
+        if (timer.get() > 10 && timer.get() < 14) {
+            Drivetrain.drivePeriodic(-0.5, 0);
+        } else {
+            Drivetrain.drivePeriodic(0, 0);
+        }
+    }
+
+    @Override
+    public void teleopInit() {
+        forward = 0;
+        turn = 0;
+
+        manualOverride = false;
+    }
+
+    @Override
+    public void teleopPeriodic() {
+        
+        
     }
 
 
--- src/main/java/frc/robot/subsystems/Motors.java
+++ src/main/java/frc/robot/subsystems/Motors.java
@@ -2,8 +2,8 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import edu.wpi.first.wpilibj.Timer;
 import frc.robot.Constants;
-import frc.robot.subsystems.*;
 
 public class Motors{
     public static VictorSPX intakeBelt;
@@ -11,6 +11,7 @@ public class Motors{
     public static VictorSPX turretAim;
     public static VictorSPX turretLoad;
     public static VictorSPX elevator;
+    public static Timer motorTimer;
 
     public static void init(){
         intakeBelt = new VictorSPX(Constants.CAN.intakeBelt);
@@ -20,11 +21,32 @@ public class Motors{
         elevator = new VictorSPX(Constants.CAN.elevator);
     }
 
-    public static return aimTurret(double angle) {
+    public static boolean aimTurret() {
+        double angle = Vision.getCurrentAngle();
         if (Vision.getTargetFound() && angle <= -Constants.angleTolerance){
             turretAim.set(ControlMode.PercentOutput, Constants.aimSpeed);
+            return false;
         } else if (Vision.getTargetFound() && angle >= Constants.angleTolerance){
             turretAim.set(ControlMode.PercentOutput, -Constants.aimSpeed);
+            return false;
+        } else if (Vision.getTargetFound()) {
+            turretFly.set(ControlMode.PercentOutput, 1);
+            return true;
         }
+        return false;
+    }
+
+    public static void loadTurret(boolean on) {
+        if (on) {
+            intakeBelt.set(ControlMode.PercentOutput, 1);
+            turretLoad.set(ControlMode.PercentOutput, 1);
+        } else {
+            intakeBelt.set(ControlMode.PercentOutput, 0);
+            turretLoad.set(ControlMode.PercentOutput, 0);
+        }
+    }
+
+    public static void flyOff() {
+        turretFly.set(ControlMode.PercentOutput, 0);
     }
 }
